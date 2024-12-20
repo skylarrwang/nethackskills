030433cd52a9a7cf2661d5b9adf244256cb966d0
skylarrwang <112836102+skylarrwang@users.noreply.github.com>
2024-12-20 17:44:23 -0500

    update to return obs and reward

--- NetPlay/skill_wrapper.py
+++ NetPlay/skill_wrapper.py
@@ -23,7 +23,7 @@ LOG_FOLDER = "/Users/srwang/Documents/pathways_fall/logs/rl_skills"
 ## Initialize the agent - use handcrafted agent, then we will call their skill methods
 
 def skill_step(env, agent, skill, parameters=None):# -> Tuple[Dict[str, Any], float, bool]:
-    total_reward = 0.0
+    obs, total_reward, done, info = env.reset(), 0, False, {}
     skill_map = {
             "fight": agent.fight,
             "explore": agent.explore,
@@ -36,12 +36,14 @@ def skill_step(env, agent, skill, parameters=None):# -> Tuple[Dict[str, Any], fl
     action_gen = generator(**parameters) if parameters else generator()
     
     print(f"Executing skill: {skill}, {action_gen}")
-    ## Reward?
     
     try:
-        for step in agent.fight():
+        for step in action_gen:
             if step.executed_action():
                 action_description = NLELanguageWrapper.all_nle_action_map[step.step_data.action][0]
+                data = step.step_data
+                obs, done, info = data.observation, data.done, data.info
+                total_reward += data.reward
                 parts = [f"Executed action '{action_description}'.", f"Thoughts: {step.thoughts}" if step.thoughts else None, f"Reward: {step.step_data.reward}" if step.step_data.reward else None]
                 print(" ".join([p for p in parts if p]))
             if step.is_done():
@@ -52,10 +54,10 @@ def skill_step(env, agent, skill, parameters=None):# -> Tuple[Dict[str, Any], fl
                 print(colored(f"Thinking: {step.thoughts}", "blue"))
     except KeyboardInterrupt:
         print(f"Aborting run.")
-    return  
+    return obs, total_reward, done, info
 
 
-## Try this
+## Example: use the fighting skill in one step
 if __name__ == "__main__":
     env = NethackGymnasiumWrapper(render_mode="human", autopickup=True)
     env = NethackH5PYMonitor(env, os.path.join(LOG_FOLDER, "trajectories.h5py"))
@@ -65,6 +67,5 @@ if __name__ == "__main__":
     
     ## try the fighting skill
     skill = "fight"
-    skill_step(env, agent, skill)
-
-        
+    obs, reward, done, info = skill_step(env, agent, skill)
+    print(f"Done with skill: {skill}, {reward}")
